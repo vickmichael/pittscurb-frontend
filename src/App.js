55f4533a48af450feb5007dc4d54cbb9efa4d302
Oliver <avilebroker@gmail.com>
2020-05-12 17:15:17 -0500

    Add routing and font

--- src/App.js
+++ src/App.js
@@ -1,63 +1,29 @@
 import React from 'react';
-import LeafletMap from './components/LeafletMap';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-import rawCoord from './mockData/coord-data.json';
-
-const dashes = {
-  default: '4'
-};
-
-const coordLineStyleMap = {
-  'no_parking': {
-    color:'red',
-    dashArray: null
-  },
-  'no_stopping': {
-    color:'orange',
-    dashArray: null
-  },
-  bus: {
-    color:'blue',
-    dashArray: dashes.default
-  },
-  loading: {
-    color:'orange',
-    dashArray: dashes.default
-  },
-  'metered_parking': {
-    color:'yellow',
-    dashArray: null
-  },
-  commercial: {
-    color:'green',
-    dashArray: dashes.default
-  },
-  unrestricted: {
-    color:'green',
-    dashArray: null
-  },
-  other: {
-    color:'grey',
-    dashArray: dashes.default
-  },
-  passenger: {
-    color:'purple',
-    dashArray: dashes.default
-  },
-};
-
-const processCoordData = () => {
-  return rawCoord.features.map(feat => ({
-    ...feat,
-    geometry: {
-      coordinates: feat.geometry.coordinates.map(longLat => [longLat[1], longLat[0]])
-    },
-  }))
-};
+import Management from './manage/Router';
+import NavBar from './common/NavBar';
 
 export default () => (
-  <LeafletMap
-    coordData={processCoordData()}
-    lineStyles={coordLineStyleMap}
-  />
+  <Router>
+    <NavBar></NavBar>
+    <Route path="/" exact>
+      <Link to={`/manage`}>Admin App</Link>
+      <br />
+      <Link to={`/reserve`}>Consumer App</Link>
+    </Route>
+    <Switch>
+      <Route path="/manage">
+        <Management />
+      </Route>
+      <Route path="/reserve">
+        Hello Reservation Portal
+      </Route>
+    </Switch>
+  </Router>
 );
