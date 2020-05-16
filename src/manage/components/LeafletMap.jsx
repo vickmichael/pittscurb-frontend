59cf8d5e8119810b1187c245d4ad8a1a238938de
Enyi Asonye <enyiasonye@gmail.com>
2020-05-16 18:23:33 -0500

    update leafletMap to have search bar

--- src/manage/components/LeafletMap.jsx
+++ src/manage/components/LeafletMap.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
   Map,
   Marker,
@@ -6,7 +6,7 @@ import {
   Polyline,
   Tooltip,
 } from 'react-leaflet';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 import rawCoord from '../mockData/coord-data.json';
@@ -15,6 +15,7 @@ import { getParkingSpots } from '../utils/spaceGeneration';
 
 import DrawingTool from './DrawingTool';
 import AreaLayer from './AreaLayer';
+import Autocomplete from '../../common/components/Autocomplete';
 
 const defaultLatLng = [40.4514974, -79.9902457]; // Somewhere in Pittsburgh
 
@@ -38,11 +39,29 @@ const StyledMap = styled(Map)`
   }
 `;
 
+const StyleInput = styled.div`
+form input,select {
+  height: 3.125rem;
+  padding: .875rem .75rem;
+  width: 30rem;
+  margin-top: 1rem;
+  margin-left: 1rem;
+  
+  background: #FFFFFF;
+  border: 1px solid #BFBFBF;
+  box-sizing: border-box;
+  border-radius: 4px;
+  font-size: 1rem;
+}
+`;
+
 const mapLayerKeys = ['esriWorldImagery', 'Stamen_TonerLabels'];
 
 export default () => {
   const dispatch = useDispatch();
-
+  const loaded = useRef(false);
+  const globalStateLocation = useSelector((state) => state.destination);
+  const [currentLocation, setCurrentLocation] = useState(defaultLatLng);
   const handleMouseMove = ({ latlng }) => {
     dispatch({ type: 'UPDATE_MOUSE_POSITION', value: latlng });
   };
@@ -55,68 +74,82 @@ export default () => {
     }
   };
 
+  useEffect(() => {
+    if (loaded.current) {
+      setCurrentLocation([globalStateLocation.geometry.location.lat(), globalStateLocation.geometry.location.lng()])
+    } else {
+      loaded.current = true
+    }
+  }, [globalStateLocation])
+
   return (
-    <StyledMap
-      id="mapId"
-      center={defaultLatLng}
-      zoom={zoom}
-      zoomControl={false}
-      onClick={handleMapClick}
-      onMouseMove={handleMouseMove}
-      onKeyDown={handleKeyDown}
-    >
+    <>
+      <StyleInput>
+        <form>
+          <Autocomplete placeholder="Search address" />
+        </form>
+      </StyleInput>
+      <StyledMap
+        id="mapId"
+        center={currentLocation}
+        zoom={zoom}
+        zoomControl={false}
+        onClick={handleMapClick}
+        onMouseMove={handleMouseMove}
+        onKeyDown={handleKeyDown}
+      >
+        {mapLayerKeys.map((key) => (
+          <TileLayer
+            maxNativeZoom={mapSources[key].maxNativeZoom}
+            maxZoom={21}
+            ext="png"
+            key={key}
+            url={mapSources[key].url}
+            attribution={false ? mapSources[key].attribution : ''}
+            style={{ opacity: 0.1 }}
+          />
+        ))}
 
-      {mapLayerKeys.map((key) => (
-        <TileLayer
-          maxNativeZoom={mapSources[key].maxNativeZoom}
-          maxZoom={21}
-          ext="png"
-          key={key}
-          url={mapSources[key].url}
-          attribution={false ? mapSources[key].attribution : ''}
-          style={{ opacity: 0.1 }}
-        />
-      ))}
+        {processCoordData.map((line, i) => {
+          const spots = getParkingSpots(line, 0);
 
-      {processCoordData.map((line, i) => {
-        const spots = getParkingSpots(line, 0);
-
-        return (
-          <>
-            <Polyline
-              key={`line${i}`}
-              weight={8}
-              lineCap="square"
-              dashArray={lineStyles[line.properties.category].dashArray}
-              color={lineStyles[line.properties.category].color}
-              positions={[line.geometry.coordinates]}
-            >
-              <Tooltip>
-                {Object.keys(line.properties).map((property) => (
-                  <div key={property}>
-                    <span>
-                      {property}
+          return (
+            <>
+              <Polyline
+                key={`line${i}`}
+                weight={8}
+                lineCap="square"
+                dashArray={lineStyles[line.properties.category].dashArray}
+                color={lineStyles[line.properties.category].color}
+                positions={[line.geometry.coordinates]}
+              >
+                <Tooltip>
+                  {Object.keys(line.properties).map((property) => (
+                    <div key={property}>
+                      <span>
+                        {property}
                       :
                       {' '}
-                    </span>
-                    <span>{line.properties[property]}</span>
-                  </div>
-                ))}
-              </Tooltip>
-            </Polyline>
-            {spots.length
-              ? (
-                lineStyles[line.properties.category].color === 'green'
-                || lineStyles[line.properties.category].color === 'yellow'
-              ) && spots.map((spot, ind) => (
-                <Marker key={`spot${ind}`} position={spot} />
-              )) : null}
-          </>
-        );
-      })}
+                      </span>
+                      <span>{line.properties[property]}</span>
+                    </div>
+                  ))}
+                </Tooltip>
+              </Polyline>
+              {spots.length
+                ? (
+                  lineStyles[line.properties.category].color === 'green'
+                  || lineStyles[line.properties.category].color === 'yellow'
+                ) && spots.map((spot, ind) => (
+                  <Marker key={`spot${ind}`} position={spot} />
+                )) : null}
+            </>
+          );
+        })}
 
-      <DrawingTool />
-      <AreaLayer />
-    </StyledMap>
+        <DrawingTool />
+        <AreaLayer />
+      </StyledMap>
+    </>
   );
 };
