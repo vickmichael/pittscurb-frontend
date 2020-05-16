4b7fde3ce7001e321cc51d18f7e12a40923d5323
ericbaumann <24817191+ericbaumann@users.noreply.github.com>
2020-05-15 22:15:25 -0400

    fixed reducers for consumer autocomplete

--- src/reserve/reducers/rootReducer.js
+++ src/reserve/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import spotSearchReducer from './spotSearchReducer';
 
 const rootReducer = combineReducers({
+  spotSearch: spotSearchReducer,
 });
 
 export default rootReducer;
