7c8cb49c0defa85652f5b2945c915ab75faeddd5
sambskn <sambaskin@ou.edu>
2020-05-16 18:34:07 -0500

    PARK-154 Creatign initial confirmation page w/test data -sb

--- src/common/constants/colors.js
+++ src/common/constants/colors.js
@@ -1,4 +1,13 @@
 export default {
   primary: '#8D1EF8',
+
+  white: '#FFFFFF',
+  grayLight: '#7C7C7C',
+  gray: '#5F5F5F',
   grayDark: '#4C4C4C',
+  black: '#2B2B2B',
+
+  success: '#07B343',
+  warning: '#EDA130',
+  failure: '#DF663F',
 };
