3c0a7d0760ed42464ce3db1af4fd9fea716937cc
Oliver <avilebroker@gmail.com>
2020-05-17 11:27:18 -0500

    fix important index in the matrix

--- src/reserve/components/Confirmation/ReservationStatus.jsx
+++ src/reserve/components/Confirmation/ReservationStatus.jsx
@@ -14,9 +14,12 @@ const WARNING_THRESHOLD = 5;
 const colorFromDiff = (start, end, now) => {
   if ((now - start) < 0) { // if reservation hasn't started
     return colors.grayDark;
-  } if ((now - end) > 0) { // if reservation is expired
+  };
+
+  if ((now - end) > 0) { // if reservation is expired
     return colors.failure;
   }  // if reservation is in progress
+
   const minRemaining = Math.ceil((end - now) / (1000 * 60));
   return minRemaining > WARNING_THRESHOLD ? colors.success : colors.warning;
 };
