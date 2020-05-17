b8b3066e9ba0f0f4405be5df48db96a384a033e3
ericbaumann <24817191+ericbaumann@users.noreply.github.com>
2020-05-16 20:05:23 -0400

    fixed merge conflicts

--- src/reserve/Router.jsx
+++ src/reserve/Router.jsx
@@ -10,6 +10,7 @@ import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import ReservationPortal from './components/ReservationPortal/ReservationPortal';
 import ReservationSearchPortal from './components/ReservationSearch/ReservationSearchPortal';
+import Confirmation from './components/Confirmation/Confirmation';
 
 const store = createStore(
   rootReducer,
@@ -25,6 +26,7 @@ export default () => {
       <Switch>
         <Route path={`${match.url}/`} exact component={ReservationPortal} />
         <Route path={`${match.url}/search`} exact component={ReservationSearchPortal} />
+        <Route path={`${match.url}/confirm/:id`} exact component={Confirmation} />
       </Switch>
     </Provider>
   );
